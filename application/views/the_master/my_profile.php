14592f3e50a323cb84b66df6f7d3255afc3fcf2a
Hendriansah Hendriansah <hendriansah@live.com>
2011-03-30 15:11:14 +0700

    Change design to fluid

--- application/views/the_master/my_profile.php
+++ application/views/the_master/my_profile.php
@@ -14,17 +14,21 @@
 									  });
 							   });
 </script>
-<div class="formbox span-24">
-	<div class="formboxtitle" style="float:left;width:930px;">
-        <div class="span-17">
+<?=form_open('the_master/my_profile');?>
+<div class="formbox penuh">
+	<div class="formboxtitle">
+        <div>
         	My Profile
         </div>
-        <div class="span-6 last" style="text-align:right;">
-            <img src="<?=base_url();?>blueprint/images/ajax_start.gif" id="loader_image" /> <a href="#" class="savebtn" id="save">Save</a> 
-        </div>
+        
 	</div>
-    <div class="span-7">
-    	<div class="formboxitem"?
+    <div class="t_r_even">
+        <div class="formboxitem">
+        	<input type="submit" value="save" class="savebtn" name="save" /> 
+        </div>
+    </div>
+    <div class="separo">
+    	<div class="formboxitem">
             <label>Username</label><br />
             <input type="text" name="username" value="<?=$row->username;?>" id="username">
         </div>
@@ -32,8 +36,7 @@
 	        <label>Name</label><br />
     	    <input type="text" name="nama_lengkap" value="<?=$row->nama_lengkap;?>" id="nama_lengkap">
         </div>
-    </div>
-    <div class="span-7 border">
+   
     	<div class="formboxitem">
 	    	<label>New Password ?</label><br />
     	    <input type="text" name="password" id="password">
@@ -44,7 +47,7 @@
         </div>
         
     </div>
-    <div class="span-10 last">
+    <div class="separo last">
     	<div class="formboxitem">
     	
         	type new password if you want to change the password, leave it blank if want to keep old password
@@ -52,5 +55,5 @@
         
     </div>
 </div>
-
+<?=form_close();?>
 <? $this->load->view('the_master/footer');?>
