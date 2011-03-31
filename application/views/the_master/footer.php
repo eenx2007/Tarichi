912b92456339acce2ca485ad7b4bf967c810c6ca
Hendriansah Hendriansah <hendriansah@live.com>
2011-03-31 17:22:46 +0700

    Repair the image url

--- application/views/the_master/footer.php
+++ application/views/the_master/footer.php
@@ -4,7 +4,7 @@
         <div id="footer">
    		<div id="footer_content" class="container">
         	<div id="footer_kiri" class="span-12">
-            	<img src="<?php echo base_url();?>blueprint/images/footer_logo.png" /><br />
+            	<img src="<?php echo base_url();?>assets/blueprint/images/footer_logo.png" /><br />
                 <?php echo $this->benchmark->elapsed_time();?> <?php echo $this->benchmark->memory_usage();?>
             </div>
             <div id="footer_kanan" class="span-12 last">
