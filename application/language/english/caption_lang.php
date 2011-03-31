8c454bf508fa7a4cb4403f35175c49ea13cbdd31
Hendriansah Hendriansah <hendriansah@live.com>
2011-03-31 22:26:33 +0700

    add some caption

--- application/language/english/caption_lang.php
+++ application/language/english/caption_lang.php
@@ -3,13 +3,18 @@
 $lang['caption_btd']="Back to Dashboard";
 $lang['caption_general_configuration']="General Configuration";
 $lang['caption_basic_cms']="Basic CMS";
+
+/*left menu caption */
 $lang['caption_site_configuration']="Site Configuration";
 $lang['caption_module_management']="Module Management";
+
+/*site config caption*/
 $lang['caption_site_name']="Site Name";
 $lang['caption_site_slogan']="Site Slogan";
 $lang['caption_date_format']="Date Format";
 $lang['caption_default_keywords']="Default Keywords";
 $lang['caption_default_descriptions']="Default Descriptions";
+$lang['caption_extra_config']="Extra Config";
 $lang['caption_language']="Language";
 $lang['caption_home_page_type']="Home Page Type";
 $lang['caption_total_post_per_page']="Total Post Per Page";
@@ -17,5 +22,18 @@ $lang['caption_site_status']="Site Status";
 $lang['caption_comment_moderation']="Comment Moderation";
 $lang['caption_main_email']="Main Email";
 $lang['caption_total_char_to_split_post']="Total Character to split post";
-$lang['caption_save']="Save";
 
+/*module management caption */
+$lang['caption_name']="Name";
+$lang['caption_ver']="Ver";
+$lang['caption_function']="Function";
+$lang['caption_type']="Type";
+$lang['caption_description']="Description";
+$lang['caption_status']="Status";
+$lang['caption_auto_activated']="Auto Activated";
+
+/*buttons*/
+$lang['caption_save']="Save";
+$lang['caption_saving']="Saving";
+$lang['caption_logged_as']="Logged as";
+$lang['caption_edit_profile']="Edit Profile";
--- application/language/indonesia/caption_lang.php
+++ application/language/indonesia/caption_lang.php
@@ -3,5 +3,37 @@
 $lang['caption_btd']="Kembali ke Dasbor";
 $lang['caption_general_configuration']="Konfigurasi Umum";
 $lang['caption_basic_cms']="Sistem Manajemen Konten Dasar";
+
+/*left menu caption */
 $lang['caption_site_configuration']="Konfigurasi Situs";
-$lang['caption_module_management']="Kelola Modul";
+$lang['caption_module_management']="Manajemen Modul";
+
+/*site config caption*/
+$lang['caption_site_name']="Nama Situs";
+$lang['caption_site_slogan']="Slogan";
+$lang['caption_date_format']="Format Tanggal";
+$lang['caption_default_keywords']="Katakunci Standar";
+$lang['caption_default_descriptions']="Deskripsi Standar";
+$lang['caption_extra_config']="Konfiruasi Lainnya";
+$lang['caption_language']="Bahasa";
+$lang['caption_home_page_type']="Tipe Halaman Utama";
+$lang['caption_total_post_per_page']="Jumlah Postingan Per Halaman";
+$lang['caption_site_status']="Status Situs";
+$lang['caption_comment_moderation']="Moderasi Komentar";
+$lang['caption_main_email']="Email Utama";
+$lang['caption_total_char_to_split_post']="Total karakter untuk dipotong";
+
+/*module management caption */
+$lang['caption_name']="Nama";
+$lang['caption_ver']="Ver";
+$lang['caption_function']="Fungsi";
+$lang['caption_type']="Tipe";
+$lang['caption_description']="Deskripsi";
+$lang['caption_status']="Status";
+$lang['caption_auto_activated']="Aktif Otomatis";
+
+/*buttons*/
+$lang['caption_save']="Simpan";
+$lang['caption_saving']="Menyimpan";
+$lang['caption_logged_as']="Masuk sebagai";
+$lang['caption_edit_profile']="Ubah Profil";
